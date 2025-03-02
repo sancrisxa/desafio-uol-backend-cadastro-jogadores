cd96de0cd5318d6de83bdf31546cb704825ac9e5
Samuel Xavier <sancrisxa@gmail.com>
2025-03-02 18:46:18 -0300

    Criacao dos modelos

---
+++ src/main/java/br/com/sancrisxa/desafio_uol_backend_cadastro_jogadores/model/GrupoCodinome.java
@@ -0,0 +1,25 @@
+package br.com.sancrisxa.desafio_uol_backend_cadastro_jogadores.model;
+
+public enum GrupoCodinome {
+    VINGADORES("Vingadores",
+            "https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/vingadores.json"),
+    LIGA_DA_JUSTICA("Liga da Justiça",
+            "https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml");
+
+    GrupoCodinome(String nome, String uri) {
+        this.nome = nome;
+        this.uri = uri;
+    }
+
+    private final String nome;
+    private final String uri;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+}
