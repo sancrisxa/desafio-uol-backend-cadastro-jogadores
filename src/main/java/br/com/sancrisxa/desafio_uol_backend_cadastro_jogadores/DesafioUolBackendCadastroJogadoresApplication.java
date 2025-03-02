f757a1ebe08a2df252babe48fc724ecf7b96e6a7
Samuel Xavier <sancrisxa@gmail.com>
2025-03-02 18:38:57 -0300

    first commit

---
+++ src/main/java/br/com/sancrisxa/desafio_uol_backend_cadastro_jogadores/DesafioUolBackendCadastroJogadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.desafio_uol_backend_cadastro_jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUolBackendCadastroJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUolBackendCadastroJogadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sancrisxa/desafio_uol_backend_cadastro_jogadores/DesafioUolBackendCadastroJogadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.desafio_uol_backend_cadastro_jogadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUolBackendCadastroJogadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
